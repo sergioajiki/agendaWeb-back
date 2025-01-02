abc2c02164545905c8313ed758364a89b004a0da
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-02 16:43:39 -0300

    feat: HealthController - página criada para teste de api

---
+++ src/main/java/com/project/agendaWeb/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.project.agendaWeb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public static String sayHello(){
+        return "AgendaWeb - Success!!";
+    }
+}
