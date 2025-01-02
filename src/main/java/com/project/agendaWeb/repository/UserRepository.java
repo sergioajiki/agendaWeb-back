1551d7fce87d7a22d22aecccf895800bf4c55599
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-02 20:33:04 -0300

    feat: UserRepository

---
+++ src/main/java/com/project/agendaWeb/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.project.agendaWeb.repository;
+
+import com.project.agendaWeb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
