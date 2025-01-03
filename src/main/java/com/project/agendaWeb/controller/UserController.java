98b5aedf6bb71d2e88eba27f420e01454024bdd1
Sergio Ajiki <sergioajk@hotmail.com>
2025-01-02 23:06:18 -0300

    feat: UserController

---
+++ src/main/java/com/project/agendaWeb/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.project.agendaWeb.controller;
+
+import com.project.agendaWeb.entity.User;
+import com.project.agendaWeb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> cadastrarUsuario(@RequestBody User user) {
+        try {
+            User novoUsuario = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+}
